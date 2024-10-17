6241d1a5b529ed13f5d42450382b57d32540f7e5
Filipe Baptista <filipe@virtuleap.com>
2024-10-17 14:15:06 +0100

    Implement Unity HDRP scene connected to TD project via extOSC plugin

---
+++ Assets/OscCore/Runtime/Scripts/Component/Message Handlers/OscFloatMessageHandler.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace OscCore
+{
+    [AddComponentMenu("OSC/Input/Float Input")]
+    public class OscFloatMessageHandler : OscMessageHandler<float, FloatUnityEvent>
+    {
+        protected override void ValueRead(OscMessageValues values)
+        {
+            m_Value = values.ReadFloatElement(0);
+            Debug.Log(m_Value);
+        }
+    }
+}
